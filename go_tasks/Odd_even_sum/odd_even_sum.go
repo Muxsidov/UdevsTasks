6c0afd591c1f2fb030c44fb2d037e8058b2b7c54
Mirzohid <56025069+Muxsidov@users.noreply.github.com>
2021-06-23 22:39:26 +0500

    Update odd_even_sum.go

--- go_tasks/Odd_even_sum/odd_even_sum.go
+++ go_tasks/Odd_even_sum/odd_even_sum.go
@@ -1,1 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	evensum, oddsum := 0, 0
+	end := 10 // How many numbers you want ?
+
+	for i := 1; i < end; i++ {
+
+		if i%2 == 0 {
+			evensum += i
+		} else {
+			oddsum += i
+		} 
+	}
+	fmt.Printf("evensum: %v \noddsum: %v\n", evensum, oddsum)
+}
 
