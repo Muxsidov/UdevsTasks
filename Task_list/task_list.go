abea93e5e607f9cb6add4f29ab50a1f55af842ae
Mirzohid <56025069+Muxsidov@users.noreply.github.com>
2021-07-26 15:11:54 +0500

    Update task_list.go

--- Task_list/task_list.go
+++ Task_list/task_list.go
@@ -1,1 +1,29 @@
+package main
+
+// importing required packages
+import (
+	"fmt"
+)
+
+type id struct {
+	id int
+}
+
+type task_list struct {
+	name     string
+	deadline string
+}
+
+
+
+func main() {
+	id_0 := id{0}
+	task1 := task_list{name: "Declare an array", deadline: "01.22.2020"}
+
+	//var mp map[id]task_list
+
+	sample := map[id]task_list{id_0: task1}
+	fmt.Print(sample)
+
+}
 
