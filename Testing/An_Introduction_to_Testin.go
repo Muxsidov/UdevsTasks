4e3341bdc0b6c0bc28847f64c5f8e99464cd7544
Mirzokhid <56025069+Muxsidov@users.noreply.github.com>
2021-07-31 21:43:49 +0500

    Create An_Introduction_to_Testin.go

---
+++ Testing/An_Introduction_to_Testin.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Calculate returns x + 2.
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func TestCalculate(t *testing.T) {
+    if Calculate(2) != 4 {
+        t.Error("Expected 2 + 2 to equal 4")
+    }
+}
+
+func TestTableCalculate(t *testing.T) {
+    var tests = []struct {
+        input    int
+        expected int
+    }{
+        {2, 4},
+        {-1, 1},
+        {0, 2},
+        {-5, -3},
+        {99999, 100001},
+    }
+
+    for _, test := range tests {
+        if output := Calculate(test.input); output != test.expected {
+            t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+        }
+    }
+}
+
+func main() {
+	fmt.Println("Hello World")
+}
