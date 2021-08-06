32c68fa838d8ec2b80da492acfe81b301be0f31e
Muxsidov <muxsidov@gmailcom>
2021-08-06 11:01:05 +0500

    con_mansql.go is working example

--- PostgreSQL_tasks/Contact_list_sql/cont_mansql.go
+++ PostgreSQL_tasks/Contact_list_sql/cont_mansql.go
@@ -20,6 +20,16 @@ const (
 
 func main() {
 
+	// Create struct for SELECT *
+	type Contact struct {
+		ID int
+		Name string 
+		Gender string 
+		Number int 
+		Email string
+		
+	}	
+	
 	// Creating the connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -30,7 +40,58 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+	
+	sqlStatement := `SELECT * FROM contacts WHERE id=$1;`
+	var contact Contact 
+	row := db.QueryRow(sqlStatement, 4)
+	err_0 := row.Scan(&contact.ID, &contact.Name, &contact.Gender,
+		&contact.Number, &contact.Email)
+	switch err_0 {
+		case sql.ErrNoRows:
+			fmt.Println("No rows were returned!")
+			return	
+		case nil:
+			fmt.Println(contact)
+		default:
+			panic(err_0)	
+	}
 
+	/*
+	// Updating 
+	sqlStatement := `
+	UPDATE contacts
+	SET name = $2, gender = $3, phone = $4, email = $5
+	WHERE id = $1
+	RETURNING id, name;`
+	//res, err := db.Exec(sqlStatement, 3, "Changed name", "chander", 1234, "changedmail@mail.com")
+	var name string
+	var id int
+	err = db.QueryRow(sqlStatement, 2, "Changed name", "chander", 1234, "changedmail@mail.com").Scan(&id, &name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(id, name)	
+	*/
+	
+	/*
+	// Deleting 
+	sqlStatement := `
+	DELETE FROM contacts
+	WHERE id = $1;`
+	res, err := db.Exec(sqlStatement, 3)
+	if err != nil {
+		panic(err)
+	}
+	
+	// Cheking if Updating/Deleting was succsessful 
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(count)
+	*/
+	
+	/*
 	// Inserting
 	sqlStatement := `
 	INSERT INTO contacts (name, gender, phone, email)
@@ -42,6 +103,7 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("New record ID is:", id)
+	*/
 
 	/*
 		// Actually opening up a connection
