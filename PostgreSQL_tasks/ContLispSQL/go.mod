20467ca1ad7ac6afb33dd5f1e5f6d92a715b0c48
Muxsidov <muxsidov@gmailcom>
2021-08-06 13:45:46 +0500

    Working Contact list pSQL

---
+++ PostgreSQL_tasks/ContLispSQL/go.mod
@@ -0,0 +1,5 @@
+module github.com/Muxsidov/udevs_tasks/tree/main/PostgreSQL_tasks/ContLispSQL
+
+go 1.16
+
+require github.com/lib/pq v1.10.2 // indirect
--- PostgreSQL_tasks/ContLispSQL/main.go
+++ PostgreSQL_tasks/ContLispSQL/main.go
@@ -27,14 +27,12 @@ type Contact struct {
 	Mail   string
 }
 
-
-
 func Menu() {
 	fmt.Println("**************************")
 	fmt.Println("*          Menu          *")
 	fmt.Println("**************************")
 
-	fmt.Println("Select a contact     - 1")
+	fmt.Println("List of contacts     - 1")
 	fmt.Println("Add new contact      - 2")
 	fmt.Println("Update a contact     - 3")
 	fmt.Println("Delete a contact     - 4")
@@ -42,37 +40,124 @@ func Menu() {
 	fmt.Println("**************************")
 }
 
-func EnterDetails(c *Contact) {
-	var phone int
-	var name, gender, mail string
-
-	fmt.Print("Enter name: ")
-	fmt.Scanln(&name)
-	fmt.Print("Enter gender: ")
-	fmt.Scanln(&gender)
-	fmt.Print("Enter phone: ")
-	fmt.Scanln(&phone)
-	fmt.Print("Enter mail: ")
-	fmt.Scanln(&mail)
-
-	c.Name = name
-	c.Gender = gender
-	c.Phone = phone
-	c.Mail = mail
-}
-
 func Add(c *Contact) {
+	// Creating the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Opening and checking if we opened connector correctly
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	// Inserting
 	sqlStatement := `
 	INSERT INTO contacts (name, gender, phone, email)
 	VALUES ($1, $2, $3, $4)
 	RETURNING id`
 	id := 0
-	err = db.QueryRow(sqlStatement, "John", "male", 5550004, "john@email.com").Scan(&id)
+	err = db.QueryRow(sqlStatement, &c.Name, &c.Gender, &c.Phone, &c.Mail).Scan(&id)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("New record ID is:", id)
+}
+
+func Update(c *Contact) {
+
+	// Creating the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Opening and checking if we opened connector correctly
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Updating
+	sqlStatement := `
+	UPDATE contacts
+	SET name = $2, gender = $3, phone = $4, email = $5
+	WHERE id = $1
+	RETURNING id, name;`
+	var name string
+	var id int
+	err = db.QueryRow(sqlStatement, &c.Id, &c.Name, &c.Gender, &c.Phone, &c.Mail).Scan(&id, &name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(id, name)
+}
+
+func Delete(c *Contact) {
+
+	// Creating the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Opening and checking if we opened connector correctly
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Deleting
+	sqlStatement := `
+	DELETE FROM contacts
+	WHERE id = $1;`
+	res, err := db.Exec(sqlStatement, &c.Id)
+	if err != nil {
+		panic(err)
+	}
+
+	// Cheking if Updating/Deleting was succsessful
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(count)
+
+}
+
+func ContactList() {
+
+	// Creating the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Opening and checking if we opened connector correctly
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, name, phone, email FROM contacts LIMIT $1", 3)
+	if err != nil {
+		// handle this error better than this
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id, phone int
+		var name, email string
+		err = rows.Scan(&id, &name, &phone, &email)
+		if err != nil {
+			// handle this error
+			panic(err)
+		}
+		fmt.Println(id, name, phone, email)
+	}
+	// get any error encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func EnterDetails(c *Contact) {
@@ -96,20 +181,8 @@ func EnterDetails(c *Contact) {
 
 func main() {
 
-	// Creating the connection string
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
-	host, port, user, password, dbname)
-
-	// Opening and checking if we opened connector correctly
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
 	var choice int
 	var id int
-	var cm ContactManager
 	var c Contact
 
 	for {
@@ -118,31 +191,21 @@ func main() {
 		fmt.Scan(&choice)
 
 		if choice == 1 {
-			cm.ContactList()
+			ContactList()
 		} else if choice == 2 {
-			c.Id = len(cm.contacts)
 			EnterDetails(&c)
-			cm.Add(&c)
+			Add(&c)
 		} else if choice == 3 {
 			fmt.Print("Enter id: ")
 			fmt.Scanln(&id)
-
-			if id <= len(cm.contacts)-1 {
-				c.Id = id
-				EnterDetails(&c)
-				cm.Update(&c)
-			} else {
-				fmt.Println("Entered id does not exists.")
-			}
+			c.Id = id
+			EnterDetails(&c)
+			Update(&c)
 		} else if choice == 4 {
 			fmt.Print("Enter id: ")
 			fmt.Scanln(&id)
-
-			if id <= len(cm.contacts)-1 {
-				cm.Delete(id)
-			} else {
-				fmt.Println("Entered id does not exists.")
-			}
+			c.Id = id
+			Delete(&c)
 		} else if choice == 5 {
 			break
 		}
